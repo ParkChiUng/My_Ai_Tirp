29d85ba758012346d334e8ef51f15fe460703dab
YOON <yys7517@naver.com>
2024-02-27 19:45:53 +0900

    [UI] Splash Theme - Color Primary, Secondary값 추가 ui/#7

---
+++ app/src/main/java/com/sessac/myaitrip/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.sessac.myaitrip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+    }
+}
