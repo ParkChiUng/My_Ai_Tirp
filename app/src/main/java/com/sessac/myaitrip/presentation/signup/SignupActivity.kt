fbe1f2d38585ba93d6c6cbe1abce873d7a30ad01
YOON <yys7517@naver.com>
2024-03-05 18:42:30 +0900

    [UI] 회원가입 화면 구성 ui/#28

---
+++ app/src/main/java/com/sessac/myaitrip/presentation/signup/SignupActivity.kt
@@ -0,0 +1,11 @@
+package com.sessac.myaitrip.presentation.signup
+
+import android.os.Bundle
+import com.sessac.myaitrip.databinding.ActivitySignupBinding
+import com.sessac.myaitrip.presentation.common.ViewBindingBaseActivity
+
+class SignupActivity : ViewBindingBaseActivity<ActivitySignupBinding>({ActivitySignupBinding.inflate(it)}) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
