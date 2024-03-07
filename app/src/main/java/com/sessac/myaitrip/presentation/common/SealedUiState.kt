3ed53cbc53a73fd417a553247c11b72f3fa79bf1
YOON <yys7517@naver.com>
2024-03-08 00:41:05 +0900

    [REFACTOR] Sealed UiState Error부분 메세지도 확인할 수 있도록 수정 feature/#27

--- app/src/main/java/com/sessac/myaitrip/presentation/common/SealedUiState.kt
+++ app/src/main/java/com/sessac/myaitrip/presentation/common/SealedUiState.kt
@@ -6,6 +6,6 @@ sealed class UiState<out T> {
     data object Empty: UiState<Nothing>()
     data object Loading: UiState<Nothing>()
     data class Success<T>(val data: T) : UiState<T>()
-    data class Error(val exception: Throwable): UiState<Nothing>()
+    data class Error(val exception: Throwable? = null, val errorMessage: String? = null): UiState<Nothing>()
     data class FirebaseAuthError(val firebaseAuthException: FirebaseAuthException): UiState<Nothing>()
 }
