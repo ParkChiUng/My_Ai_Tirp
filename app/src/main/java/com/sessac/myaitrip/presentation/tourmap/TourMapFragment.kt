aa766d40c116071f26043a777e141ccef26f9ad0
YOON <yys7517@naver.com>
2024-03-13 15:54:12 +0900

    [CHORE] Init NaverMapView feature/#42

--- app/src/main/java/com/sessac/myaitrip/presentation/tourmap/TourMapFragment.kt
+++ app/src/main/java/com/sessac/myaitrip/presentation/tourmap/TourMapFragment.kt
@@ -1,25 +1,66 @@
 package com.sessac.myaitrip.presentation.tourmap
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 import com.sessac.myaitrip.databinding.FragmentTourMapBinding
 import com.sessac.myaitrip.presentation.common.ViewBindingBaseFragment
 
 /**
  * 맵 페이지
  */
-class TourMapFragment :
-    ViewBindingBaseFragment<FragmentTourMapBinding>(FragmentTourMapBinding::inflate) {
+class TourMapFragment
+    : ViewBindingBaseFragment<FragmentTourMapBinding>(FragmentTourMapBinding::inflate),
+    OnMapReadyCallback {
 
-    private lateinit var mapBinding: FragmentTourMapBinding
+    private lateinit var mapView: MapView
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        mapBinding = FragmentTourMapBinding.inflate(inflater, container, false)
-        return mapBinding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView = binding.mapViewTourMap.also {
+            it.onCreate(savedInstanceState)
+        }
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
