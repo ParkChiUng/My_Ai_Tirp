a0d78745cb42b4640d3bb62947752433761d7d8f
YOON <yys7517@naver.com>
2024-03-20 18:01:19 +0900

    [FEAT] 커스터마이즈된 OkHttp 클라이언트로 Glide TimeOut 설정 feature/#42

--- app/src/main/java/com/sessac/myaitrip/GlideModule.kt
+++ app/src/main/java/com/sessac/myaitrip/GlideModule.kt
@@ -2,15 +2,22 @@ package com.sessac.myaitrip
 
 import android.content.Context
 import android.util.Log
+import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool
 import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
 import com.bumptech.glide.load.engine.cache.LruResourceCache
 import com.bumptech.glide.load.engine.cache.MemorySizeCalculator
+import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
+import okhttp3.OkHttpClient
+import java.io.InputStream
+import java.util.concurrent.TimeUnit
 
 @GlideModule
 class GlideModule: AppGlideModule() {
@@ -39,7 +46,16 @@ class GlideModule: AppGlideModule() {
         }
     }
 
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        val builder = OkHttpClient.Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+
+        // 커스터마이즈된 OkHttp 클라이언트로 Glide 설정
+        registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(builder.build()))
+    }
+
     override fun isManifestParsingEnabled(): Boolean {
-        return super.isManifestParsingEnabled()
+        return false
     }
 }
